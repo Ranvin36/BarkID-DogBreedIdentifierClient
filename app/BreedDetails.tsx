6d0ad10b681b1db8407ab85e1a3d5a5dedec86c4
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2024-12-28 03:32:14 +0530

    BreedDetails-UI-Update & API Intergration

--- app/BreedDetails.tsx
+++ app/BreedDetails.tsx
@@ -1,9 +1,42 @@
-import { StyleSheet, View } from "react-native"
+import { Image, StyleSheet, TouchableOpacity, View,Text} from "react-native"
+import { dummy } from "@/assets/dummy"
+import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
+import axios from "axios"
+import { useEffect } from "react"
 
 const BreedDetails:React.FC = () =>{
+    async function GetBreedDetails(){
+        const response = await axios.get('https://api.api-ninjas.com/v1/dogs?name=golden retriever',{
+            headers:{
+                "x-api-key":"0RGWKoTvLHcv3JgAhR7vpw==QFCxDjpIp9PQV9W2"
+            }
+        })
+        console.log(response.data)
+
+    }
+
+    useEffect(() =>{
+        GetBreedDetails()
+    },[])
     return(
         <View style={styles.container}>
-            
+            <View style={styles.imageHeader}>
+                <Image source={{uri:dummy[0].image}} style={styles.imageBg}/>
+                <TouchableOpacity style={styles.arrow} onPress={() => router.back()}>
+                    <MaterialIcons name="keyboard-arrow-left" size={22} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.breedInfo}>
+                <View>
+                    <Text style={{fontFamily:"Poppins-Bold",fontSize:19}}>Golden Retriever</Text>
+                    <Text style={{color:"#ccc",fontFamily:"Poppins-Light",fontSize:12,marginTop:-5}}>The Predicted Breed Has A Percentage Of 70%</Text>
+                </View>
+                <View style={{marginVertical:10}}>
+                    <Text style={{fontFamily:"Poppins-Light"}}>The Caucasian Shepherd dog is a serious guardian breed and should never be taken lightly.</Text>
+                </View>
+            </View>
+
         </View>
     )
 }
@@ -14,6 +47,31 @@ export default BreedDetails
 const styles = StyleSheet.create({
     container:{
         height:"100%",
-        backgroundColor:"#f2f2f2"
+        paddingHorizontal:10,
+        backgroundColor:"#fff"
+    },
+    imageHeader:{
+        marginVertical:5,
+        position:"relative"
+    },
+    imageBg:{
+        width:"100%",
+        borderRadius:40,
+        height:350
+    },
+    arrow:{
+        position:"absolute",
+        left:20,
+        top:20,
+        backgroundColor:"#f2f2f2",
+        width:30,
+        height:30,
+        borderRadius:50,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    breedInfo:{
+        marginVertical:10,
+        marginHorizontal:5
     }
 })
--- assets/dummy.ts
+++ assets/dummy.ts
@@ -1,3 +1,5 @@
 export const dummy =[
-    {id:1,name:"Golden Retriever",image:"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2020/07/09151754/Golden-Retriever-puppy-standing-outdoors.jpg"}
+    {id:1,name:"Golden Retriever",image:"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2020/07/09151754/Golden-Retriever-puppy-standing-outdoors.jpg"},
+    {id:2,name:"German Shepherd",image:"https://www.bellaandduke.com/wp-content/uploads/2024/10/A-guide-to-German-Shepherds-characteristics-personality-lifespan-and-more-featured-image.webp"},
+    {id:3,name:"German Shepherd",image:"https://www.bellaandduke.com/wp-content/uploads/2024/10/A-guide-to-German-Shepherds-characteristics-personality-lifespan-and-more-featured-image.webp"},
 ]
